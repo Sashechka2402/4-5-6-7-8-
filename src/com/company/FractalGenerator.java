1ab7391a402f632a1c40fc3215db37b033ebbf14
Alexandra Vinokurova <your_email@whatever.comvinokurova02@bk.ru>
2021-05-07 16:00:13 +0300

    4 laba

---
+++ src/com/company/FractalGenerator.java
@@ -0,0 +1,67 @@
+package com.company;
+
+
+import java.awt.geom.Rectangle2D;
+
+
+//Этот класс предоставляет общий интерфейс и операции для фракталов.
+//генераторы, которые можно просмотреть в Fractal Explorer.
+
+public abstract class FractalGenerator {
+
+    /**
+     * This static helper function takes an integer coordinate and converts it
+     * into a double-precision value corresponding to a specific range.  It is
+     * used to convert pixel coordinates into double-precision values for
+     * computing fractals, etc.
+     *
+     * @param rangeMin the minimum value of the floating-point range
+     * @param rangeMax the maximum value of the floating-point range
+     *
+     * @param size the size of the dimension that the pixel coordinate is from.
+     *        For example, this might be the image width, or the image height.
+     *
+     * @param coord the coordinate to compute the double-precision value for.
+     *        The coordinate should fall in the range [0, size].
+     */
+
+    public static double getCoord(double rangeMin, double rangeMax,
+                                  int size, int coord) {
+
+        assert size > 0;
+        assert coord >= 0 && coord < size;
+
+        double range = rangeMax - rangeMin;
+        return rangeMin + (range * (double) coord / (double) size);
+    }
+
+
+    // позволяет генератору фракталов определить наиболее «интересную»
+    // область комплексной плоскости для конкретного фрактала
+
+    public abstract void getInitialRange(Rectangle2D.Double range);
+
+
+    // Обновляет текущий диапазон, чтобы он был центрирован по указанным координатам,
+    // и для увеличения или уменьшения с указанным коэффициентом масштабирования.
+
+    public void recenterAndZoomRange(Rectangle2D.Double range,
+                                     double centerX, double centerY, double scale) {
+
+        double newWidth = range.width * scale;
+        double newHeight = range.height * scale;
+
+        range.x = centerX - newWidth / 2;
+        range.y = centerY - newHeight / 2;
+        range.width = newWidth;
+        range.height = newHeight;
+    }
+
+    // реализует итеративную функцию для фрактала Мандельброта
+    //Учитывая координату <em> x </em> + <em> iy </em> на комплексной плоскости,
+    //вычисляет и возвращает количество итераций перед фракталом функция выходит за пределы ограничивающей
+    // области для этой точки. Дело в том, что не исчезает, пока не будет указан предел итераций с результатом -1.
+
+    public abstract int numIterations(double x, double y);
+
+}
